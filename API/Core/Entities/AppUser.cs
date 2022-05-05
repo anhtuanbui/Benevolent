f0a991cbd30b235e7b2b633796e27c0a992fba62
Tuan <anhtuan128@gmail.com>
2022-05-05 17:10:38 +0700

    fix bug not adding feedback

--- API/Core/Entities/AppUser.cs
+++ API/Core/Entities/AppUser.cs
@@ -7,6 +7,6 @@ namespace API.Core.Entities
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Address { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
     }
 }
